82eecefa0e976da3aa49fb1b8cfb8cfc6896e323
Igor Bari <mucsi96@gmail.com>
2026-03-12 13:02:50 +0100

    Fix tag URL in deploy script and remove trailing slash from Azure Key Vault endpoint

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,8 +18,8 @@ trap 'rm -f "$KUBECONFIG_FILE"' EXIT
 hostname=$(az keyvault secret show --vault-name $VAULT_NAME --name hostname --query value --output tsv)
 apiClientId=$(az keyvault secret show --vault-name $VAULT_NAME --name api-client-id --query value --output tsv)
 # Get latest tags for both server and client
-serverLatestTag=$(curl -s "https://registry.hub.docker.com/v2/repositories/mucsi96/skeleton-app-server/tags/" | jq -r '.results | map(select(.name != "latest")) | sort_by(.last_updated) | reverse | .[0].name')
-clientLatestTag=$(curl -s "https://registry.hub.docker.com/v2/repositories/mucsi96/skeleton-app-client/tags/" | jq -r '.results | map(select(.name != "latest")) | sort_by(.last_updated) | reverse | .[0].name')
+serverLatestTag=$(curl -s "https://registry.hub.docker.com/v2/repositories/mucsi96/skeleton-app-server/tags" | jq -r '.results | map(select(.name != "latest")) | sort_by(.last_updated) | reverse | .[0].name')
+clientLatestTag=$(curl -s "https://registry.hub.docker.com/v2/repositories/mucsi96/skeleton-app-client/tags" | jq -r '.results | map(select(.name != "latest")) | sort_by(.last_updated) | reverse | .[0].name')
 
 echo "Updating Helm repositories..."
 helm repo add mucsi96 https://mucsi96.github.io/k8s-helm-charts --force-update
@@ -39,7 +39,7 @@ helm upgrade hello-server mucsi96/spring-app \
     --set basePath=/api \
     --set clientId=$apiClientId \
     --set serviceAccountName=hello-api-workload-identity \
-    --set env.AZURE_KEYVAULT_ENDPOINT=https://$VAULT_NAME.vault.azure.net/ \
+    --set env.AZURE_KEYVAULT_ENDPOINT=https://$VAULT_NAME.vault.azure.net \
     --set env.STORAGE_DIRECTORY=/app/storage \
     --set persistentVolumeClaims[0].name=hello-pvc \
     --set persistentVolumeClaims[0].accessMode=ReadWriteOnce \
